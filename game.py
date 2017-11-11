f171e7526c14afb28ce308f47371ea923cef3400
jfuex <jfuex@USAUNJFUEX2>
2017-11-11 16:29:11 -0600

    Fixed add players problem

--- game.py
+++ game.py
@@ -2,24 +2,26 @@ import cards
 import players
 
 class Game(object):
+    deck = cards.Deck()
+    
     def __init__(self):
         cards_per_hand = 5
         current_Player = None
         deck = cards.Deck()
-        players= []
+        game_players= []
 
-    def start_game():
+    def start_game(self):
         #check number of players
-        player_count = len(players)
+        player_count = len(game_players)
         if player_count==4: 
             deck.shuffle()
         
-            for player in players:
+            for player in game_players:
                 player.hand = deck.deal_cards(cards_per_hand)
         else:
             print("You need four players for this game. You have {}".format(player_count))
         
-    def add_player(player):
-        players.append(player)
+    def add_player(self,player):
+        game_players.append(player)
 
 
